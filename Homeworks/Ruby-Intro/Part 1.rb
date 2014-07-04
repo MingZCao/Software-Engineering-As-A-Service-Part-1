34e5b77ca307f9e25c5a3287094acdda6212df88
MingZCao <caomingzhi300@gmail.com>
2014-07-04 00:41:37 -0700

    Update Part 1.rb

--- Homeworks/Ruby-Intro/Part 1.rb
+++ Homeworks/Ruby-Intro/Part 1.rb
@@ -1,27 +1,23 @@
-#Andrew Hawker
-#SaaS - Armando Fox & David Patterson (Spring 2012)
-#Assignment 1 - Part 1
-#http://spark-university.s3.amazonaws.com/berkeley-saas/homework/hw1.pdf
-
-#(1a)
-#Write a method that determines whether a given word or phrase is
-#a palindrome, that is, it reads the same backwards as forwards, ignoring
-#case, punctuation, and nonword characters.
-def palindrome?(string)
-  return false if string.nil?
-
-  s = string.gsub(/\W/, '')
-  srev = s.reverse
-  eq = s.casecmp srev
-  eq == 0
+ 
+# PART 1
+ 
+# Define a method sum which takes an array of integers as an argument and returns the sum of its elements. For an empty array it should return zero.
+def sum(integers)
+  integers.reduce(:+) || 0
 end
-
-#(1b)
-#Given a string of input, return a hash whose keys are words in the string
-#and whos values are the number of times each word appears.
-def count_words(string)
-  return nil if string.nil?
-
-  w = string.split(/\b/).map {|x| x.gsub(/[\W]+/, '').downcase}.reject {|x| x.empty?}
-  Hash[w.map {|x| [x, w.select {|y| x == y}.size]}]
+ 
+# Define a method max_2_sum which takes an array of integers as an argument and returns the sum of its two largest elements. For an empty array it should return zero. For an array with just one element, it should return that element.
+ 
+def max_2_sum(integers)
+  return integers.first if integers.length == 1
+  integers.sort.pop(2).reduce(:+) || 0
 end
+ 
+# Define a method sum_to_n? which takes an array of integers and an additional integer, n, as arguments and returns true if any two distinct elements in the array of integers sum to n. An empty array or single element array should both return false.
+ 
+def sum_to_n?(integers, sum)
+  return false if integers.length <= 1
+  integers.uniq.combination(2).map{ |pair| pair.reduce(:+) }.include?(sum)
+end
+ 
+ 
